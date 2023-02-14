4eaccdfe1f272f60afcc134a3a39f0fd95801697
ColtonFindley <123335067+ColtonFindley@users.noreply.github.com>
2023-02-14 11:52:14 -0700

    HW03 work

--- CS2060-Class-Code-main/Homework/HW02.c
+++ CS2060-Class-Code-main/Homework/HW02.c
@@ -2,12 +2,13 @@
 // This code will simulate a parking garage, with how many cars and what the fees are
 
 #include <stdio.h>
-#define MINIMUM_FEE 2.00 // Beginning fee
-#define HOUR_FEE 0.75 // Fee for every hour over the minimum
-#define MAXIMUM_CHARGE 10.00 // Largest charge available
-#define FREE_HOUR 3 // Hours that get no extra charge
+#define MIN_FLAT_RATE_CHARGE 3.00 // Beginning fee
+#define ADDITIONAL_HOURS_RATE 0.75 // Fee for every hour over the minimum
+#define  MAX_CHARGE 12.00 // Largest charge available
+#define MIN_HOURS_AT_FLAT_RATE 3 // Hours that get no extra charge
+#define MAX_HOURS_ALLOWED 24 // Maximum amount of hours
+double getValidNumber(double hours);
 double calculations(double charge); // Calculates the charge
-void display(int count, double hours, double charge); // Displays cars, hour, and charge
 void endDisplay(int count, double totalHours, double totalCharge); // Displays the total cars, hours, and charge
 
 int main(void) {
@@ -23,25 +24,22 @@ int main(void) {
 	validNumber = scanf("%lf", &hours); // Asking user for a double 
 	while ((getchar()) != '\n'); // Clearing the buffer
 
-	// If hours was less than 0 or more than 24, excluding -1
-	while (hours == 0 || hours != -1 && hours < -1 || hours > 24) {
-		puts("Invalid");
-		validNumber = scanf("%lf", &hours); // Asking user for a double 
-		while ((getchar()) != '\n'); // Clearing the buffer
-	}
-
 	// Loop untill the user enters a -1
 	while (hours != -1) {
 
+		hours = getValidNumber(hours);
+		validNumber = 1;
+
 		// If user entered a valid number
-		if (validNumber == 1) {
+		if (validNumber == 1 && hours != -1) {
 
 			charge = calculations(hours); // Calculating the charge
 			totalCharge = totalCharge + charge;
 			totalHours = totalHours + hours;
 			
 			count++;
-			display(count, hours, charge);
+			puts("Car          Hours         Charge");
+			printf("%-12d %-13.1f $%.2f\n", count, hours, charge);
 			
 			// Prompts the user for another input
 			puts("Enter the number of hours the car was parked or enter -1 to quit.");
@@ -49,7 +47,7 @@ int main(void) {
 			while ((getchar()) != '\n'); // Clearing the buffer
 		}
 		// If user entered an invalid number
-		else if (validNumber == 0) {
+		else if (validNumber == 0 && hours == -1) {
 			puts("You did not enter a number");
 
 			// Prompting the user for another input
@@ -62,29 +60,36 @@ int main(void) {
 
 } // End main
 
+double getValidNumber(double hours) {
+	// If hours was less than 0 or more than 24, excluding -1
+	int validNumber = 0;
+	while (hours == 0 || hours != -1 && hours < -1 || hours > MAX_HOURS_ALLOWED || validNumber == 0) {
+		puts("You did not enter a number");
+		puts("Enter the number of hours the car was parked or enter -1 to quit.");
+		validNumber = scanf("%lf", &hours); // Asking user for a double 
+		while ((getchar()) != '\n'); // Clearing the buffer
+	}
+	return hours;
+}
+
 double calculations(double charge) {
 	
-	double finalCharge = MINIMUM_FEE;
-	charge = charge - FREE_HOUR;
+	double finalCharge = MIN_FLAT_RATE_CHARGE;
+	charge = charge - MIN_HOURS_AT_FLAT_RATE;
 
 	// Loop for adding the additional charge
 	while (charge > 0) {
-		finalCharge = finalCharge + HOUR_FEE;
+		finalCharge = finalCharge + ADDITIONAL_HOURS_RATE;
 		charge--;
 	}
 	// If charge is over the maximum
-	if (finalCharge > MAXIMUM_CHARGE) {
-		finalCharge = MAXIMUM_CHARGE;
+	if (finalCharge > MAX_CHARGE) {
+		finalCharge = MAX_CHARGE;
 	}
 
 	return finalCharge;
 
 } // calculations
-
-void display(int count, double hours, double charge) {
-	puts("Car          Hours         Charge");
-	printf("%-12d %-13.1f %.2f\n", count, hours, charge);
-} // display
 
 void endDisplay(int count, double totalHours, double totalCharge) {
 	puts("Parking Garage Summary");
