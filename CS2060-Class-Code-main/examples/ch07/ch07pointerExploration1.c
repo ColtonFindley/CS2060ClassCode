f92abb653f68b069b8e72fb55e67aad45c489ef6
ColtonFindley <123335067+ColtonFindley@users.noreply.github.com>
2023-02-25 15:36:47 -0700

    GE04

--- CS2060-Class-Code-main/examples/ch07/ch07pointerExploration1.c
+++ CS2060-Class-Code-main/examples/ch07/ch07pointerExploration1.c
@@ -10,31 +10,31 @@
 int main ()
 {
 	// Initalize houseNum
-	int houseNum = 13;
+	int houseNum = 13; // Int value of 13
 	int calcHouseNum1 = 0;
 	int calcHouseNum2 = 0;
-	int *houseNumPtr = &houseNum;
+	int *houseNumPtr = &houseNum; // Pointer to houseNum
 	
-	printf("houseNum %d\n", houseNum);
+	printf("houseNum %d\n", houseNum); // Prints int value
 
-	printf("&houseNum %p\n\n", &houseNum);
+	printf("&houseNum %p\n\n", &houseNum); // Prints the address
 
-	printf ("*houseNumPtr  %d\n", *houseNumPtr);
+	printf ("*houseNumPtr  %d\n", *houseNumPtr); // Print 13
 
-	printf ("houseNumPtr %p\n\n", houseNumPtr);
+	printf ("houseNumPtr %p\n\n", houseNumPtr); // Pointers store the address of another value
 
-	printf ("&houseNumPtr %p\n\n", &houseNumPtr);
+	printf ("&houseNumPtr %p\n\n", &houseNumPtr); // Pointers have a seperate address
 
-	calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
+	calcHouseNum1 =  *houseNumPtr + *houseNumPtr; // The * makes it a value
 
 	printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
 
-	calcHouseNum2 = 2 * (*houseNumPtr);
+	calcHouseNum2 = 2 * (*houseNumPtr); // *houseNumPtr = 13
 
 	printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
 
 	printf("\n&*houseNumPtr = %p"
-		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
+		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr); // Both do the same thing
 			
 	return 0;
 }
--- CS2060-Class-Code-main/examples/ch07/ch07sec7_4CubePassRef.c
+++ CS2060-Class-Code-main/examples/ch07/ch07sec7_4CubePassRef.c
@@ -5,22 +5,41 @@
 #include <stdlib.h>
 
 int cubePass1(int number);
-int cubePass2(int * numberPtr);
+void cubePass2(int * numberPtr);
+void func1(const int* const numberPtr);
+void func2(const int* const numberPtr);
 
 int main(void) {
 
-	int numberMain = 5;
-	int result = 0;
+	int numberMain = 5; // Pass by value
+	int result = 0; // Return value
+
+	int test = 33;
+	int test1 = 3;
+	int test2 = 10;
+	int test3 = 0;
+	int test4 = -2;
+
+	int *array[5] = { &test, &test1, &test2, &test3, &test4 };
+	for (int i = 0; i < 5; i++) {
+		printf("array[%d} %p\n", i, array[i]);
+		printf("*array[%d] %p\n", i, array[i]);
+	}
 	
 	printf ("In main before cubePass1  numberMain = %d\n", numberMain);
 	printf("&numberMain = %p\n", &numberMain);
 	result = cubePass1(numberMain);
 	printf ("In main after cubePass1  numberMain = %d\n", numberMain);
 	printf ("Result = %d\n", result);
+
 	printf("\nIn main before cubePass2  numberMain = %d\n", numberMain);
-	result = cubePass2(&numberMain);
+	cubePass2(&numberMain);
 	printf("\nIn main after cubePass2  numberMain = %d\n", numberMain);
 	printf("result = %d\n", result);
+
+	func1(&numberMain);
+	func2(&numberMain);
+
 
 } // main
 
@@ -29,26 +48,36 @@ int cubePass1 (int number)
 {
 	int cube = 0;
 	puts("\nIn cubePass1");
-	printf("number = %d\n", number);
-	printf("&number = %p\n", &number);
-	cube = number * number * number;
-	printf("cube  = %d\n", cube);
-	number = cube;
-	printf("number = %d\n", number);
+	printf("number = %d\n", number); // 5
+	printf("&number = %p\n", &number); // Address
+	cube = number * number * number; // 5 * 5 * 5
+	printf("cube  = %d\n", cube); // Prints 125
+	number = cube; // numberMain is not affected outside of cubePass1
+	printf("number = %d\n", number); // = cube
 	return cube;
 } 
 
-int cubePass2 (int * numberPtr) 
-{
-	int cube = 0;
-	puts ("\nIn cubePass2");
-	printf("numberPtr = %p\n", numberPtr);
-	printf ("*numberPtr = %d\n", *numberPtr);
-	printf("&numberPtr = %p\n", &numberPtr);
-	cube = (* numberPtr )* (* numberPtr )* (* numberPtr); 
+void cubePass2(int * const numberPtr) {
+
+	printf("   *numberPtr = %d\n", *numberPtr);
+
+	printf("   numberPtr = %p\n", numberPtr);
+
+	int cube = (*numberPtr) * (*numberPtr) * (*numberPtr);
+
 	*numberPtr = cube;
-	printf ("*numberPtr = %d\n", *numberPtr);
-	return cube;
-} 
 
+	*numberPtr = cube;
+
+	printf("   *numberPtr = %d\n\n", *numberPtr);
+
+}
+
+void func1(const int* numberPtr) {
+	printf("   *numberPtr = %d\n\n", *numberPtr);
+}
+
+void func2(const int* const numberPtr) {
+	printf("   *numberPtr = %d\n\n", *numberPtr);
+}
 
